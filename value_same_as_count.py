394e663591831a8782b9a6e41e47d0a4fe483b2f
nivas ponthapalli <99869774+NIVASPONTHAPALLI@users.noreply.github.com>
2023-05-23 09:55:30 +0530

    from collections import Counter n=int(input()) ar=list(map(int,input().split())) arr1=Counter(ar) cnt=0 for i in arr1.keys():     if i==arr1[i]:         cnt+=1 print(cnt)

---
+++ value_same_as_count.py
@@ -0,0 +1,10 @@
+from collections import Counter
+n=int(input())
+ar=list(map(int,input().split()))
+arr1=Counter(ar)
+cnt=0
+for i in arr1.keys():
+    if i==arr1[i]:
+        cnt+=1
+print(cnt)
+    
