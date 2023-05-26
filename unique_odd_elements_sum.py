63fdeee34fdcb6b061353d351953d8bb36b414b5
nivas ponthapalli <99869774+NIVASPONTHAPALLI@users.noreply.github.com>
2023-05-26 12:09:39 +0530

    n=int(input()) lst=list(map(int,input().split())) l=[] cnt=0 for i in range(n):     if lst[i] not  in l:         l.append(lst[i]) for j in range(len(l)):     if l[j]%2!=0:         cnt+=l[j] print(cnt)

---
+++ unique_odd_elements_sum.py
@@ -0,0 +1,11 @@
+n=int(input())
+lst=list(map(int,input().split()))
+l=[]
+cnt=0
+for i in range(n):
+    if lst[i] not  in l:
+        l.append(lst[i])
+for j in range(len(l)):
+    if l[j]%2!=0:
+        cnt+=l[j]
+print(cnt)
