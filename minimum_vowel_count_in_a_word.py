f63565978859b138f1e77c2131b66ec6f5fe939d
nivas ponthapalli <99869774+NIVASPONTHAPALLI@users.noreply.github.com>
2023-05-27 14:29:18 +0530

    def vowel(i):     l1=0     for j in i:         if j in s:             l1+=1     return l1 n=input() p=n.split() s='aeiouAEIOU' l=[] for i in p:     p=vowel(i)     l.append(p) print(min(l))

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,14 @@
+def vowel(i):
+    l1=0
+    for j in i:
+        if j in s:
+            l1+=1
+    return l1
+n=input()
+p=n.split()
+s='aeiouAEIOU'
+l=[]
+for i in p:
+    p=vowel(i)
+    l.append(p)
+print(min(l))
