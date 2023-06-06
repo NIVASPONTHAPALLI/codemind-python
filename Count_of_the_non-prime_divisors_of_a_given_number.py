6952a7866973cda33daacdb76497639c1a769df4
nivas ponthapalli <99869774+NIVASPONTHAPALLI@users.noreply.github.com>
2023-06-06 13:50:16 +0530

    import math def prime(n):     if n<2:         return False     for i in range(2,int(math.sqrt(n))+1):         if n%i==0:             return False     return True n=int(input()) c=[] for i in range(1,n+1):     if n%i==0:         c.append(i) cv=0 for i in c:     if prime(i)==False:         cv+=1 print(cv)

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,21 +1,18 @@
 import math
-def prime(m):
-    if m<2:
+def prime(n):
+    if n<2:
         return False
-    for i in range(2,int(math.sqrt(m))+1):
-        if m%i==0:
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
             return False
     return True
 n=int(input())
-l=[]
-l2=[]
+c=[]
 for i in range(1,n+1):
     if n%i==0:
-        l.append(i)
-for j in l:
-    if prime(j):
-        l2.append(j)
-for k in l2:
-    if k in l:
-        l.remove(k)
-print(len(l))
+        c.append(i)
+cv=0
+for i in c:
+    if prime(i)==False:
+        cv+=1
+print(cv)
