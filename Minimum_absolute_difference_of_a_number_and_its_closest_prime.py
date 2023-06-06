17ee0ac668a82f5bec27831b03bbd5c22f99852d
nivas ponthapalli <99869774+NIVASPONTHAPALLI@users.noreply.github.com>
2023-06-06 15:13:07 +0530

    import math def prime(n):     if n<2:         return False     for i in range(2,int(math.sqrt(n))+1):         if n%i==0:             return False     return True n=int(input()) s=len(str(n)) c=0 cv=0 for i in range(n,-1,-1):     if prime(i)==True:         c=i         break for j in range(n,n+100):     if prime(j)==True:         cv=j         break p=abs(n-cv) p1=abs(n-c) if p<p1:     print(p) else:     print(p1)

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,26 @@
+import math
+def prime(n):
+    if n<2:
+        return False
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            return False
+    return True
+n=int(input())
+s=len(str(n))
+c=0
+cv=0
+for i in range(n,-1,-1):
+    if prime(i)==True:
+        c=i
+        break
+for j in range(n,n+100):
+    if prime(j)==True:
+        cv=j
+        break
+p=abs(n-cv)
+p1=abs(n-c)
+if p<p1:
+    print(p)
+else:
+    print(p1)
