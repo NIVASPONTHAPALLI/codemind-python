af10da52835434a1cd2d0d86a8e887e7d3384752
nivas ponthapalli <99869774+NIVASPONTHAPALLI@users.noreply.github.com>
2023-06-06 15:06:14 +0530

    import math def prime(n):     if n<2:         return False     for i in range(2,int(math.sqrt(n))+1):         if n%i==0:             return False     return True n=int(input()) s=len(str(n)) if prime(n)!=True:     print('Not Mega Prime')
    else:
        c=0
        while n!=0:
            p=n%10
            n=n//10
            if prime(p)==True:
                c+=1
            else:
                print('Not Mega Prime')
                break
        if c==s:
            print('Mega Prime')

--- mega_prime.py
+++ mega_prime.py
@@ -1,25 +1,25 @@
-def prime_num(n):
-    c=0
-    for i in range(1,n+1):
-        if(n%i==0):
-            c+=1
-    if(c==2):
-        return 1
-    else:
-        return 0
+import math
+def prime(n):
+    if n<2:
+        return False
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            return False
+    return True
 n=int(input())
 s=len(str(n))
-c=0
-if(prime_num(n)==0):
-    print("Not Mega Prime")
+if prime(n)!=True:
+    print('Not Mega Prime')
+  
 else:
-    while n>0:
-        r=n%10
+    c=0
+    while n!=0:
+        p=n%10
         n=n//10
-        if(prime_num(r)==1):
+        if prime(p)==True:
             c+=1
         else:
-            print("Not Mega Prime")
+            print('Not Mega Prime')
             break
-    if(c==s):
-        print("Mega Prime")
+    if c==s:
+        print('Mega Prime')
